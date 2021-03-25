25b959d97c1dd9202761a60c42b4d257912734f8
sang-rak <sangrack114@naver.com>
2021-03-26 01:24:28 +0900

    0326

---
+++ reactnative/AwesomeProject/app/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  StyleSheet, 
+  Text, 
+  View ,
+} from 'react-native';
+
+const Main = () => {
+
+  return (
+    <view> 
+      <Text>Test</Text>  
+    </view>
+  );
+};
+
+export default Main;
---
+++ tidy/sql.md
@@ -0,0 +1,14 @@
+생성
+
+python manage.py sqlmigrate users 0001
+
+json 적용
+
+python manage.py loaddata users/users.json
+
+SQL문 Print
+
+python manage.py shell_plus --print-sql
+
+
+
