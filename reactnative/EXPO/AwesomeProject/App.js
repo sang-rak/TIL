0db4b2f522b80f48df71ebe87f0740a4655fbac8
sang-rak <sangrack114@naver.com>
2021-03-30 22:48:35 +0900

    0330

--- reactnative/EXPO/AwesomeProject/App.js
+++ reactnative/EXPO/AwesomeProject/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Main from './app/components/Main';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Main />
-    </View>
+    <Main />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
